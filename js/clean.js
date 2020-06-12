4b705cba18dad6a4c58262205c9eb4c0ca11c201
Jason Jiang <jasonjiangny@gmail.com>
2020-06-12 17:25:27 -0400

    brb taking a shower

--- js/clean.js
+++ js/clean.js
@@ -1,63 +1,26 @@
-$(document).ready(function(){
-   // set inital values
-   $('.email').hide()
-   $('.cpwd').hide()
+function initial(){
+   // only show submit image
    $('#Guide').hide()
-})
-
-function switchNew(){
-   $('#formArea').hide('slow', function(){
-       $('#heading').text('New Users')
-       $('#whichForm').val('Choice')
-       $('#switch').text('Returning Users')
-                   .attr('onclick', 'switchOld()')
-       $('#submit').text('Make Account')
-       $('#forgotPwd').hide()
-       $('.email').show()
-       $('.cpwd').show()
-       $('#formArea').show('slow')
-   })
-   
-   
-
 }
-function switchOld(){
-   $('#formArea').hide('slow', function(){
-       $('#heading').text('Login')
-       $('#whichForm').val('Auto')
-       $('#switch').text('New User')
-                   .attr('onclick', 'switchNew()')
-       $('#submit').text('Login')
-       $('.email').hide()
-       $('.cpwd').hide()
-       $('#forgotPwd').show()
-       $('#formArea').show('slow')
-   })
-   
-}
-$("#form").submit(function(event){ 
-//credit to https://www.sanwebe.com/2016/07/ajax-form-submit-examples-using-jquery
-   event.preventDefault(); //prevent default action 
-   var passward = $('#pwd').val()
-   var cpassward = $('#cpwd').val()
-   if (passward == cpassward){
-       var post_url = $(this).attr("action"); //get form action url
-       var request_method = $(this).attr("method"); //get form GET/POST method
-       var form_data = $(this).serialize(); //Encode form elements for submission
-       
-       $.ajax({
-           url : main.py,
-           type: POST,
-           data : form_data
-       }).done(function(response){ //work on feedback div later
-           $('#responseBox').html(response)
-       })
-   }else{
-       $('#responseBox').html('Passwards must match. Please Try Again')
-       
+$(document).ready(initial())
+var stage = 0
+
+// hide / show objects based on stage
+function showStage(step){ 
+   stage = stage + step
+   if (stage == 0) {
+      initial()
    }
+   else if (stage == 1){
+      
+   }
+}
+function move(step){
+   showStage(stage + step)
+   
+   
+}
 
-});
 var guide = false
 function toggleGuide(){
    if (guide == false){
@@ -83,3 +46,27 @@ function toggleGuide(){
       
 }
 
+
+$("#form").submit(function(event){ 
+//credit to https://www.sanwebe.com/2016/07/ajax-form-submit-examples-using-jquery
+   event.preventDefault(); //prevent default action 
+   var passward = $('#pwd').val()
+   var cpassward = $('#cpwd').val()
+   if (passward == cpassward){
+       var post_url = $(this).attr("action"); //get form action url
+       var request_method = $(this).attr("method"); //get form GET/POST method
+       var form_data = $(this).serialize(); //Encode form elements for submission
+       
+       $.ajax({
+           url : main.py,
+           type: POST,
+           data : form_data
+       }).done(function(response){ //work on feedback div later
+           $('#responseBox').html(response)
+       })
+   }else{
+       $('#responseBox').html('Passwards must match. Please Try Again')
+       
+   }
+
+});
