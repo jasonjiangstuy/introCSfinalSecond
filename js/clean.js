b62e567b399d2571232867e41d81603d07fd9f3b
Jason Jiang <jasonjiangny@gmail.com>
2020-06-13 19:37:45 -0400

    test diffrent approach with overlayed canvas

--- js/clean.js
+++ js/clean.js
@@ -58,7 +58,27 @@ function showStage(step){
                   console.log(canvas);
                   c = canvas.getContext('2d')
                   c.drawImage(blockSprite, 0, 0, blockSprite.width, blockSprite.height);
+                  blockSprite.mousemove(function(e) {
+                         
+                     if(!this.canvas) {
+                         this.canvas = $('<canvas />')[0];
+                         this.canvas.width = this.width;
+                         this.canvas.height = this.height;
+                         this.canvas.getContext('2d').drawImage(this, 0, 0, this.width, this.height);
+                     }
+                     
+                     var pixelData = this.canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
+                     
+                     $('#output').html('R: ' + pixelData[0] + '<br>G: ' + pixelData[1] + '<br>B: ' + pixelData[2] + '<br>A: ' + pixelData[3]);
+                     
+                     
+                 });
                   document.body.appendChild(blockSprite);
+                  
+                     
+                     
+                     
+                 
                }
                blockSprite.setAttribute("src", e.target.result); 
                //  .width(150)
