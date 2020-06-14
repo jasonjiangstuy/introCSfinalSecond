61362632680c7d02d33b1385df9f8bdcd33d4bde
Jason Jiang <jasonjiangny@gmail.com>
2020-06-14 16:26:11 -0400

    restest1

--- js/clean.js
+++ js/clean.js
@@ -27,6 +27,12 @@ function initial(){
    $('#dropperPanel').hide();   
 }
 
+function rgbToHex(r, g, b) {
+   if (r > 255 || g > 255 || b > 255)
+       throw "Invalid color component";
+   return ((r << 16) | (g << 8) | b).toString(16);
+}
+
 function eyeDropperMoveAction(e, which, me){
    console.log(which);
    var pixelData = me.getContext('2d').getImageData(e.offsetX, e.offsetY, 1, 1).data;
