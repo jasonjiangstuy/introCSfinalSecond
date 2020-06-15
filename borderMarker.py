76cb6fd35112945ac11b1a069a57140cbc2139bc
Jason Jiang <jasonjiangny@gmail.com>
2020-06-15 04:28:05 -0400

    push

--- borderMarker.py
+++ borderMarker.py
@@ -9,24 +9,28 @@ import math
 # marker color
 value = 0.5
 
-tolerance = 
+tolerance = 5
+
 
 import numpy as np
 def find_nearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
-    array[idx]
+    if tolerance > (array[idx] - value):
+       return False
     return array[idx]
 
-img = Image.open(fileObj)
+img = Image.open('testImgWithMarkers/difficult.jpg')
 array = np.array(img)
 print(array)
 # [ 0.21069679  0.61290182  0.63425412  0.84635244  0.91599191  0.00213826
 #   0.17104965  0.56874386  0.57319379  0.28719469]
 
+while True:
+   cornerPx = find_nearest(array, value)
+   
+   print(cornerPx)
 
-cornerPx = find_nearest(array, value)
-print(cornerPx)
 
 
 # 0.568743859261
--- main.py
+++ main.py
@@ -164,7 +164,7 @@ if isRequest:
       # img_str = base64.b64encode(buffered.getvalue())
       #  return b 64 string
       import urllib.request
-      urllib.request.urlretrieve(url, str(cookie) + ".jpg")
+      urllib.request.urlretrieve(url, str(cookie) + ".jpeg")
       
 
    except:
